375d0a383765db2b78f19db667a9d5c05e19f47e
TDMwangi <teddymwangi65@gmail.com>
2023-11-19 09:04:51 +0300

    feat: ask the user for the city

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
 #1. Create a greeting for your program.
 print("Hello there! Welcome to the band name generator!")
+
+#2. Ask the user for the city that they grew up in.
+city  = (input("First off, what city are you from?\n"))
