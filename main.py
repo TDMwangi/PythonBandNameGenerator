bbf00db2f3bdb6efc447a3fc989f42865a50964b
TDMwangi <teddymwangi65@gmail.com>
2023-11-20 22:05:42 +0300

    feat: ask the user for the name of a pet

--- main.py
+++ main.py
@@ -3,3 +3,6 @@ print("Hello there! Welcome to the band name generator!")
 
 #2. Ask the user for the city that they grew up in.
 city  = (input("First off, what city are you from?\n"))
+
+#3. Ask the user for the name of a pet.
+pet_name = (input("Alright! Now enter a name of a pet!\n"))
